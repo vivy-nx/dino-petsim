ebe9c799158948be7121670101e3d188bc4d0420
Annika Morgan <annikamorgan27@gmail.com>
2025-10-15 14:56:53 -0400

    Working Menu system
    Didn't add the credits bc idk where we got the assets from.

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public PlayerMove playerMoveScript; // assign this in the inspector
+
+    public void TitleScreen()
+    {
+        Debug.Log("Loading Title Screen...");
+
+        if (playerMoveScript != null)
+            playerMoveScript.Freeze();
+
+        SceneManager.LoadScene("Title");
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void CreditScreen()
+    {
+        SceneManager.LoadScene("CreditScreen");
+    }
+
+    public void ControlsScreen()
+    {
+        SceneManager.LoadScene("ControlScreen");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -17,9 +17,13 @@ public class PlayerMove : MonoBehaviour
     public float jumpHeight = 10f;
     bool isGrounded;
 
+    bool isFrozen = false;
+
     // Update is called once per frame
     void Update()
     {
+        if (isFrozen) return; // Skip movement while frozen
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
@@ -34,14 +38,26 @@ public class PlayerMove : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
-        
-
         Vector3 move = transform.right * moveX + transform.forward * moveZ;
-
         controller.Move(move * speed * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
-
         controller.Move(velocity * Time.deltaTime);
     }
+
+
+    //freezes the player so he doesnt shoot into the air
+    public void Freeze()
+    {
+        isFrozen = true;
+        velocity = Vector3.zero;
+    }
+
+    public void Unfreeze()
+    {
+        isFrozen = false;
+    }
+
 }
+
+
