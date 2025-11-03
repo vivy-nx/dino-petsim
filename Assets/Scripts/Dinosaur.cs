49c537c836dd0090aab47783521805751129c7ed
Vivy-NX <eneyxd@gmail.com>
2025-11-03 18:59:30 -0500

    dinosaurs now MOVE ON THEIR OWN, switching between moving and idle states, which resolves #20 (basic movement). state machine is in a little better of a state now too. added a second dinosaur to our test scene using the spinosaurus model, but for some reason it was acting really jittery compared to the brontosaurus. but very happy about this development even if it's not toooo too much functionality all in one thing

--- Assets/Scripts/Dinosaur.cs
+++ Assets/Scripts/Dinosaur.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class Dinosaur : MonoBehaviour
 {
@@ -26,6 +28,7 @@ public class Dinosaur : MonoBehaviour
         emoting,
         sleeping,
     }
+    bool enteringNewState = true;
     public species dinoSpecies = species.unknown;
     public string dinoName = "Dinosaur";
     public float age = 0f;
@@ -34,12 +37,13 @@ public class Dinosaur : MonoBehaviour
     public float happiness = 10f; //unhappiness negative? low energy unhappiness sadness and high energy unhappiness anger?
     public int energy = 100;
     public behavior behaviorState = behavior.idle;
-    public float behaviorTimer = 0; //determine how long a dinosaur will do behaviors for, sometimes
+    float behaviorTimer = 0; //determine how long a dinosaur will do behaviors for, sometimes
+    public NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        randomTimerLength();
     }
 
     // Update is called once per frame
@@ -47,28 +51,93 @@ public class Dinosaur : MonoBehaviour
     {
         age += Time.deltaTime;
         hunger += Time.deltaTime / 20;
+        behaviorTimer -= Time.deltaTime;
 
-        //control what is done every frame when state updates. probably can be broken down into smaller functions as needed but for now it can just be like this
-        switch(behaviorState)
+        //state machine
+        stateMachine();
+        
+    }
+    void stateMachine()
+    {
+        if (enteringNewState == true) //start of new state 
         {
-            case behavior.idle:
-            
-                break;
-            case behavior.looking:
+            enteringNewState = false;
+            switch (behaviorState)
+            {
+                case behavior.idle:
+                    //Debug.Log(dinoName + " idling");
+                    randomTimerLength();
+                    break;
+                case behavior.looking:
+                    randomTimerLength();
+                    agent.SetDestination(
+                        transform.position + new Vector3(
+                            UnityEngine.Random.Range(-1, 1),
+                            0,
+                            UnityEngine.Random.Range(-1, 1)
+                        )
+                    );
+                    break;
+                case behavior.moving:
+                    //Debug.Log(dinoName + " moving");
+                    //set destination to a random point on XZ
+                    float magnitude = UnityEngine.Random.Range(10, 30);
+                    agent.SetDestination(
+                        transform.position + new Vector3(
+                            UnityEngine.Random.Range(-magnitude, magnitude), 
+                            0, 
+                            UnityEngine.Random.Range(-magnitude, magnitude)
+                        )
+                    );
+                    break;
+                case behavior.eating:
 
-                break;
-            case behavior.moving:
-                
-                break;
-            case behavior.eating:
+                    break;
+                case behavior.emoting:
 
-                break;
-            case behavior.emoting:
+                    break;
+                case behavior.sleeping:
 
-                break;
-            case behavior.sleeping:
-
-                break;
+                    break;
+            }
         }
+        else //update for current state (maybe not all that much needs to be done on a per frame basis.
+        {
+            switch (behaviorState)
+            {
+                case behavior.idle:
+                    if (behaviorTimer < 0)
+                    {
+                        //will probably want to make a more complex system or get a random roll for what states are entered from idle timer ending
+                        //but for now only moving is implemented so. 
+                        ChangeState(behavior.moving);
+                    }
+                    break;
+                case behavior.moving:
+                    if (agent.remainingDistance < 0.2)
+                    {
+                        bool coinflip = (UnityEngine.Random.value <= 0.5);
+                        if (coinflip)
+                        {
+                            ChangeState(behavior.moving);
+                        } else
+                        {
+                            ChangeState(behavior.idle);
+                        }
+                    }
+                    break;
+                
+            }
+        }
+    }
+    void ChangeState(behavior newState)
+    {
+        behaviorState = newState;
+        enteringNewState = true;
+    }
+
+    void randomTimerLength()
+    {
+        behaviorTimer = UnityEngine.Random.Range(3,15);
     }
 }
