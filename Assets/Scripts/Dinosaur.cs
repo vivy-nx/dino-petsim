d855364badea066125deeb0cd10af0b8cdf094f8
Vivy-NX <eneyxd@gmail.com>
2025-12-11 20:23:29 -0500

    manually bring stuff from vs-other-history to main

--- Assets/Scripts/Dinosaur.cs
+++ Assets/Scripts/Dinosaur.cs
@@ -27,7 +27,7 @@ public class Dinosaur : MonoBehaviour
         emoting,
         sleeping,
     }
-
+    bool enteringNewState = true;
     public species dinoSpecies = species.unknown;
     public string dinoName = "Dinosaur";
     public float age = 0f;
@@ -37,11 +37,13 @@ public class Dinosaur : MonoBehaviour
     public int energy = 100;
     public behavior behaviorState = behavior.idle;
     public float behaviorTimer = 0; // determine how long a dinosaur will do behaviors for, sometimes
-
+    NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        randomTimerLength();
         // Initialize variables if necessary, but health and hunger should already be set
     }
 
@@ -50,37 +52,74 @@ public class Dinosaur : MonoBehaviour
     {
         age += Time.deltaTime;
         hunger += Time.deltaTime / 20;
-
+        behaviorTimer -= Time.deltaTime;
         HandleHealthLoss();
-
-        // control what is done every frame when state updates. probably can be broken down into smaller functions as needed but for now it can just be like this
-        switch (behaviorState)
-        {
-            case behavior.idle:
-                HandleIdleBehavior();
-                break;
-            case behavior.looking:
-                HandleLookingBehavior();
-                break;
-            case behavior.moving:
-                HandleMovingBehavior();
-                break;
-            case behavior.eating:
-                HandleEatingBehavior();
-                break;
-            case behavior.emoting:
-                HandleEmotingBehavior();
-                break;
-            case behavior.sleeping:
-                HandleSleepingBehavior();
-                break;
-        }
-
+        //state machine
+        stateMachine();
         // clamp functions ( i explain it later i just added this last )
         health = Mathf.Clamp(health, 0f, 100f);
         happiness = Mathf.Clamp(happiness, 0f, 100f);
         hunger = Mathf.Clamp(hunger, 0f, 100f);
         energy = Mathf.Clamp(energy, 0, 100);
+    }
+    void stateMachine()
+    {
+        if (enteringNewState == true) //start of new state 
+        {
+            enteringNewState = false;
+            switch (behaviorState)
+            {
+                case behavior.idle:
+                    HandleIdleBehavior();
+                    break;
+                case behavior.looking:
+                    HandleLookingBehavior();
+                    break;
+                case behavior.moving:
+                    HandleMovingBehavior();
+                    break;
+                case behavior.eating:
+                    HandleEatingBehavior();
+                    break;
+                case behavior.emoting:
+                    HandleEmotingBehavior();
+                    break;
+                case behavior.sleeping:
+                    HandleSleepingBehavior();
+                    break;
+            }
+        }
+        else //update for current state (maybe not all that much needs to be done on a per frame basis.
+        {
+            switch (behaviorState)
+            {
+                case behavior.idle:
+                    if (behaviorTimer < 0)
+                    {
+                        //will probably want to make a more complex system or get a random roll for what states are entered from idle timer ending
+                        //but for now only moving is implemented so. 
+                        ChangeState(behavior.moving);
+                    }
+                    break;
+                case behavior.moving:
+                    if (agent.remainingDistance < 0.2)
+                    {
+                        bool coinflip = (UnityEngine.Random.value <= 0.5);
+                        if (coinflip)
+                        {
+                            ChangeState(behavior.moving);
+                        }
+                        else
+                        {
+                            ChangeState(behavior.idle);
+                        }
+                    }
+                    break;
+
+            }
+        }
+
+
     }
 
     //--------------------------------------------------
@@ -89,6 +128,7 @@ public class Dinosaur : MonoBehaviour
 
     private void HandleIdleBehavior()
     {
+        randomTimerLength();
         // increase hunger and decrease happiness while dino idle
         hunger += Time.deltaTime / 10; // hunger increase 
         happiness -= Time.deltaTime / 30; // decrease happiness
@@ -96,12 +136,28 @@ public class Dinosaur : MonoBehaviour
 
     private void HandleLookingBehavior()
     {
-      
+        randomTimerLength();
+        agent.SetDestination(
+            transform.position + new Vector3(
+                UnityEngine.Random.Range(-1, 1),
+                0,
+                UnityEngine.Random.Range(-1, 1)
+            )
+        );
     }
 
     private void HandleMovingBehavior()
     {
-    
+        //Debug.Log(dinoName + " moving");
+        //set destination to a random point on XZ
+        float magnitude = UnityEngine.Random.Range(10, 30);
+        agent.SetDestination(
+            transform.position + new Vector3(
+                UnityEngine.Random.Range(-magnitude, magnitude),
+                0,
+                UnityEngine.Random.Range(-magnitude, magnitude)
+            )
+        );
     }
 
     private void HandleEatingBehavior()
@@ -173,5 +229,16 @@ public class Dinosaur : MonoBehaviour
     public void SetName(string newName)  // if we want to change the dino's name
     {
         dinoName = newName;
+    }
+
+    void ChangeState(behavior newState)
+    {
+        behaviorState = newState;
+        enteringNewState = true;
+    }
+
+    void randomTimerLength()
+    {
+        behaviorTimer = UnityEngine.Random.Range(3, 15);
     }
 }
